731af8c7a84a0c538aac74311b72e4f8547dfe27
Santiago Alonzo Aguilar <salonzo9878@gmail.com>
2025-11-04 11:26:02 -0600

    changed input method to read a txt file

--- main.cpp
+++ main.cpp
@@ -1,42 +1,74 @@
 #include <vector>
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <algorithm>
 
 using namespace std;
 
-int main(){
+int main() {
     int N;
-    cin >> N;
+    ifstream inputFile("example.txt"); 
+    if(!inputFile.is_open()){
+        cerr << "Error: Unable to open file." << endl;
+        return 1;
+    }
+
+    string line;
+    getline(inputFile, line);
+    N = stoi(line);
 
     vector<vector<int>> distance_matrix(N, vector<int>(N));
     vector<vector<int>> flow_matrix(N, vector<int>(N));
+    vector<vector<int>> polygons(N, vector<int>(2));
 
-    int d;
-    for(int i = 0; i<N; i++){
-        for(int j = 0; j<N; j++){
-            cin >> d;
-            distance_matrix[i][j] = d;
+    for(int i = 0; i < N; ++i){
+        getline(inputFile, line);
+        while (line.empty()) getline(inputFile, line);
+        stringstream ss(line);
+        for(int j = 0; j < N; ++j){
+            ss >> distance_matrix[i][j];
         }
     }
-    for(int i = 0; i<N; i++){
-        for(int j = 0; j<N; j++){
-            cin >> d;
-            flow_matrix[i][j] = d;
+
+    for(int i = 0; i < N; ++i){
+        getline(inputFile, line);
+        while (line.empty()) getline(inputFile, line);
+        stringstream ss(line);
+        for(int j = 0; j < N; ++j){
+            ss >> flow_matrix[i][j];
         }
     }
-    cout << endl;
-}
 
-/*
-for(int i = 0; i<N; i++){
-    for(int j = 0; j<N; j++){
-        cout << distance_matrix[i][j] << " ";
+    for(int i = 0; i < N; ++i){
+        getline(inputFile, line);
+        while (line.empty()) getline(inputFile, line);
+        line.erase(remove(line.begin(), line.end(), '('), line.end());
+        line.erase(remove(line.begin(), line.end(), ')'), line.end());
+        replace(line.begin(), line.end(), ',', ' ');
+        stringstream ss(line);
+        ss >> polygons[i][0] >> polygons[i][1];
     }
-    cout << endl;
-}
-for(int i = 0; i<N; i++){
-    for(int j = 0; j<N; j++){
-        cout << flow_matrix[i][j] << " ";
+    inputFile.close();
+
+    for(int i = 0; i < N; ++i){
+        for (int j = 0; j < N; ++j) {
+            cout << distance_matrix[i][j] << " ";
+        }
+        cout << endl;
     }
-    cout << endl;
+
+    for(int i = 0; i < N; ++i){
+        for (int j = 0; j < N; ++j) {
+            cout << flow_matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    for(int i = 0; i < N; ++i){
+        cout << "(" << polygons[i][0] << ", " << polygons[i][1] << ")" << endl;
+    }
+
+    return 0;
 }
-*/
